f6da1cfe4a358642e9f57b0c2137de56f685317f
Merahi Yacine <yacinekadda8@gmail.com>
2023-09-17 18:19:48 +0100

    Create featured books cubit states

---
+++ lib/Features/home/presntation/view_model/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:mybook/Features/home/data/models/book_model/book_model.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
